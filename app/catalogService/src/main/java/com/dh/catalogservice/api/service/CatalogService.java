476cd9144c5b3a4cf62117805a5c8d259ee27b96
JheziFox <francopesenda@hotmail.com>
2022-08-27 22:18:08 -0300

    clean

--- app/catalogService/src/main/java/com/dh/catalogservice/api/service/CatalogService.java
+++
@@ -1,8 +0,0 @@
-package com.dh.catalogservice.api.service;
-
-import com.dh.catalogservice.domain.model.dto.Catalog;
-
-public interface CatalogService {
-
-    Catalog findCatalogByGenre(String genre);
-}
